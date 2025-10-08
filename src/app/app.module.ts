a52c56cdb5b866fa140319df9e0326c6f25417e0
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-10-08 00:42:51 -0500

    feat(sidebar): crear componente de barra lateral con opciones y subopciones
    - Se agregó el componente <app-markenx-sidebar> y sus subcomponentes:
      - AppSidebarComponent: gestiona las opciones principales y la selección actual.
      - AppSidebarOptionComponent: representa cada opción individual con soporte para subopciones.
    - Se integró el sidebar en app.component.html junto al header.
    - Se incorporaron PrimeNG MenuModule y PanelModule para futuras expansiones del sidebar.
    - Se definieron nuevos colores y variables globales en styles.scss para soporte visual del sidebar:
      - Tonos de gris, negro y rojo con hover.
    - Se añadieron estilos específicos para opciones, subopciones y divisores activos.
    - Se implementó lógica para selección de opción activa y expansión condicional de subopciones.
    
    https://tita2-202610.atlassian.net/browse/PNJ-66

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { MenuModule } from 'primeng/menu';
+import { PanelModule } from 'primeng/panel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MarkenxHeaderComponent } from './markenx-header/markenx-header.component';
+import { AppSidebarComponent } from './markenx-sidebar/markenx-sidebar.component';
+import { AppSidebarOptionComponent } from './markenx-sidebar/sidebar-option/sidebar-option.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MarkenxHeaderComponent
+    MarkenxHeaderComponent,
+    AppSidebarComponent,
+    AppSidebarOptionComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,6 +28,8 @@ import { MarkenxHeaderComponent } from './markenx-header/markenx-header.componen
     FormsModule,
     ButtonModule,
     TableModule,
+    MenuModule,
+    PanelModule,
     AppRoutingModule
   ],
   providers: [],
